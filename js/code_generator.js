0047434143d3f8028638150847c5143b4f518575
mbauskar <mbauskar@gmail.com>
2015-10-06 01:28:20 +0530

    new js structure to call methods and generate code

--- js/code_generator.js
+++ js/code_generator.js
@@ -1,61 +1,74 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-    // Center window on screen.
-    var screenWidth = screen.availWidth;
-    var screenHeight = screen.availHeight;
-    var width = 500;
-    var height = 300;
+// chrome.app.runtime.onLaunched.addListener(function() {
+//     // Center window on screen.
+//     var screenWidth = screen.availWidth;
+//     var screenHeight = screen.availHeight;
+//     var width = 500;
+//     var height = 300;
+//
+//     chrome.app.window.create('popup.html', {
+//         id: "CodeGenID",
+//         outerBounds: {
+//             width: width,
+//             height: height,
+//             left: Math.round((screenWidth - width) / 2),
+//             top: Math.round((screenHeight - height) / 2)
+//         }
+//     });
+// });
 
-    chrome.app.window.create('popup.html', {
-        id: "CodeGenID",
-        outerBounds: {
-            width: width,
-            height: height,
-            left: Math.round((screenWidth - width) / 2),
-            top: Math.round((screenHeight - height) / 2)
-        }
-    });
-});
+window.methods = {}
+var cli = "generate_html_table_structure"
+
+var headings = {
+    "html_table":"<h4><b>HTML Tables</b></h4>",
+    "grids":"<h4><b>Bootstrap Grids</b></h4>"
+}
 
 $(document).ready(function() {
-    $('#generate_code').on('click', function(e) {
-        var rows = parseInt($("[name='rows']").val());
-        var cols = parseInt($("[name='cols']").val());
-        console.log([rows, cols])
-        if (validate_input(rows, cols)) {
-            result = generate_bootstrap_grid_structure(rows, cols);
-            // result = generate_html_table_structure(parseInt(rows), parseInt(cols));
-            copy_text(result["code"]);
-            $("#result").html(result["msg"]);
-        }
-    });
+    $("#panel-heading").html(headings["html_table"]);
+    methods.bind_events();
 });
 
-validate_input = function(rows, cols) {
-    // validate input from user
-    var result = false
+methods.bind_events = function(){
+    // Create Code
+    $('#generate_code').on('click', function(e) {
+        var rows = $("[name='rows']").val();
+        var cols = $("[name='cols']").val();
 
-    if (/^[1-9]*$/.test(rows) && /^[1-9]*$/.test(cols)) {
-        if (cols > 12)
-            $("#result").html("Number of columns per row can not exceed 12");
-        else
-            result = true
-    } else
-        $("#result").html("Invalid Input");
+        if (validate_input(rows, cols)) {
+            args = {
+                "rows":parseInt(rows),
+                "cols":parseInt(cols)
+            }
 
-    return result
+            result = methods[cli](args);
+            copy_text(result["code"]);
+            $("#result").html(result["msg"]);
+            $("#code").html(result["code"]);
+        }
+    });
+
+    $("#clear-textbox").on('click', function(e){
+        clear_code();
+    })
+
+    $("#html_table_structure").on('click', function(e){
+        cli = "generate_html_table_structure"
+        $("#panel-heading").html(headings["html_table"])
+    })
+
+    $("#bootstrap_grid_structure").on('click', function(e){
+        cli = "generate_bootstrap_grid_structure"
+        $("#panel-heading").html(headings["grids"])
+    })
 }
 
-copy_text = function(text) {
-    var $temp = $("<textarea>");
-    $("body").append($temp);
-    $temp.val(text).select();
-    document.execCommand("copy");
-    $temp.remove();
-}
+methods.generate_bootstrap_grid_structure = function(args) {
+    rows = args["rows"];
+    cols = args["cols"];
+    var code = "";
 
-generate_bootstrap_grid_structure = function(rows, cols) {
-    var code = ""
-    var msg = "Bootstrap grid structure code is copied please paste the code to text editor .."
+    var msg = "Bootstrap grid structure code is copied please paste the code to text editor ..";
     var col_cls = 12 % cols == 0 ? "col-xs-" + 12 / cols : "col-xs-";
 
     // Generate Grid Structure
@@ -73,16 +86,11 @@ generate_bootstrap_grid_structure = function(rows, cols) {
     }
 }
 
-// get_column_cls = function(cols){
-//     if(12%cols == 0)
-//         return "col-xs-"12/cols;
-//     else
-//         return "col-xs-"
-// }
-
-generate_html_table_structure = function(rows, cols) {
+methods.generate_html_table_structure = function(args) {
     var code = ""
-    var msg = "Bootstrap grid structure code is copied please paste the code to text editor .."
+    var msg = "HTML table code is copied please paste the code to text editor .."
+    rows = args["rows"];
+    cols = args["cols"];
 
     // Creating Table Stucture
     code = "<table>\n\t<thead>";
@@ -101,4 +109,41 @@ generate_html_table_structure = function(rows, cols) {
         "code": code,
         "msg": msg
     }
+}
+
+validate_input = function(rows, cols) {
+    // validate input from user
+    var result = false
+
+    if (/^[0-9]*$/.test(rows) && /^[0-9]*$/.test(cols)) {
+        cols = parseInt(cols);
+        rows = parseInt(rows);
+
+        if(rows != 0 && cols != 0){
+            if (cli == "generate_bootstrap_grid_structure" && cols > 12)
+                $("#result").html("Number of columns per row can not exceed 12");
+            else
+                result = true
+        }
+        else
+            $("#result").html("Input can not be zero");
+    } else
+        $("#result").html("Invalid Input");
+
+    return result
+}
+
+copy_text = function(text) {
+    var $temp = $("<textarea>");
+    $("body").append($temp);
+    $temp.val(text).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+
+clear_code = function(){
+    $("#code").val("");
+    // reset active form
+    $("form").trigger("reset");
+    $("#result").html("");
 }
